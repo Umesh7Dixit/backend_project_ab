e80d2070f191e3cab79cb5ae9235f25cedf29c49
Umesh7Dixit <umesh7dixit@gmail.com>
2025-11-22 22:10:57 +0530

    revert

--- modules/templates.js
+++ modules/templates.js
@@ -1970,11 +1970,11 @@ class Templates {
   getAllProjectsByOrgID  = async (req, res) => {
     try {
 
-      const { facility_id} = req.body
+      const { org_id} = req.body
 
       const query = {
-        text: 'SELECT * FROM projects WHERE facility_id = $1',
-        values: [facility_id]
+        text: 'SELECT * FROM projects WHERE org_id = $1',
+        values: [org_id]
       };
 
       const result = await this.utility.sql.query(query);
